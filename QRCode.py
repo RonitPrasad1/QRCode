19181495db9a4d9dca21a978a2d0bcf2f8588826
Ronit Prasad <55261813+RonitPrasad1@users.noreply.github.com>
2023-07-18 10:18:22 -0500

    Update QRCode.py

--- QRCode.py
+++ QRCode.py
@@ -4,17 +4,20 @@ import qrcode
 
 # Create the QRCode with the chosen data inside of it:
 def qr_code_generator() -> None:
-	myQRCode = qrcode.QRCode(
-		version=1,
-		error_correction=qrcode.constants.ERROR_CORRECT_L,
-		box_size=30,
-		border=1,
-	)
+	try:
+		myQRCode = qrcode.QRCode(
+			version=1,
+			error_correction=qrcode.constants.ERROR_CORRECT_L,
+			box_size=30,
+			border=1,
+		)
+	except Exception as e:
+		print("An error occurred:", str(e))
 
-	# Storing the HTTPS URL inside of a string:
+	# Store inside of string:
 	contentsOfQRCode = "https://drive.google.com/file/d/1fxyvc8HvMwpxci_orip8clTa1AC4E0d9/view?usp=sharing"
 
-	# Adding the data with error handling:
+	# Adding the data:
 	try:
 		myQRCode.add_data(contentsOfQRCode)
 		myQRCode.make(fit=True)
@@ -22,15 +25,14 @@ def qr_code_generator() -> None:
 		# Generating/creating the image:
 		generatedImage = myQRCode.make_image(fill_color="black", back_color="white")
 
-		# Saving the image as a "PNG" to whatever path you specify, shown below:
+		# Saving the image as a PNG to whatever path you want:
 		generatedImage.save(r'C:\Users\ronit\PycharmProjects\QRCodeProject\secure_QRCode.png')
 		print("Your QR Code has been generated!")
 	except Exception as error:
-		print("An error occurred while generating the QR code!")
-		print(error)
+		print("An error occurred while generating the QR code!", error)
 
-    # DEBUGGER: Check if the image was saved successfully:
-    '''
+	# DEBUGGER: Check if the image was saved successfully:
+	'''
     try:
         with open('qr_code.png', 'rb'):
             print("DEBUGGER: Works!")
